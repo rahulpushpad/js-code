9d97d2b0162c1ad601d00eb2baf695a3b4b932f8
Rahul Pushpad <113465865+rahulpushpad@users.noreply.github.com>
2024-09-03 08:05:49 +0000

    Discuss about var, let, const keywords

---
+++ basics_JS/keywords.js
@@ -0,0 +1,20 @@
+const employeeID=123456
+var empName="rahul"
+let emailID="rahul@g.com"
+empCity="sarangpur"
+let homeTown;
+
+console.table([employeeID,empName,emailID,empCity,homeTown]);
+
+//for some reason chnge the values
+
+employeeID=123484
+empName="rahul pushpad"
+emailID="rahul@g.com and mail.com"
+empCity="sarangpur/indore"
+homeTown;
+
+
+console.table([employeeID,empName,emailID,empCity,homeTown]);
+
+//the result is const value is not change, its constant.
