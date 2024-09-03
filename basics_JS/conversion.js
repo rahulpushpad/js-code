b97afd24d9dbe3fb23383f90c6294d66533893db
Rahul Pushpad <113465865+rahulpushpad@users.noreply.github.com>
2024-09-03 09:23:06 +0000

    Covert values like num to string and boolean

---
+++ basics_JS/conversion.js
@@ -0,0 +1,22 @@
+// let value=256
+// let value="259kjgg"
+
+let value="rahul"
+console.log(typeof value);
+
+let change = Number(value)
+
+console.log(typeof change);
+
+// console.log(change);
+
+// now string
+
+let num=5895
+console.log(typeof num);
+
+let exchange = String(num)
+
+console.log(typeof exchange);
+
+console.log(exchange);
