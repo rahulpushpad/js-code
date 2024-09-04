f49699c5ee271e5a904ed673aa1ee45da1589be7
Rahul Pushpad <113465865+rahulpushpad@users.noreply.github.com>
2024-09-04 15:18:59 +0530

    datesMethods

---
+++ basics_JS/dates.js
@@ -0,0 +1,8 @@
+// let myDate= new Date('12-02-2024');
+// console.log(myDate.toDateString());
+
+// let myDate= new Date('2024/01/5');
+// console.log(myDate.toDateString());
+
+let myDate=Date.now()
+console.log(myDate);
